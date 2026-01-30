19cd4c00dcdc16b98dc5706a33c74b4bbcdbede1
Kristian <vestboe.kristian@gmail.com>
2026-01-30 13:27:39 +0100

    Start of project

---
+++ src/main/java/edu/ntnu/idatt2003/gruppe50/App.java
@@ -0,0 +1,13 @@
+package edu.ntnu.idatt2003.gruppe50;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
