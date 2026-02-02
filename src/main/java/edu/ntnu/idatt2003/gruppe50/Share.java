5901459782c45ee8f4ee162e0f8b2ba9faa6b18a
Marius Klepp <Mariusklepp@icloud.com>
2026-02-02 15:23:14 +0100

    Share class

---
+++ src/main/java/edu/ntnu/idatt2003/gruppe50/Share.java
@@ -0,0 +1,38 @@
+package edu.ntnu.idatt2003.gruppe50;
+
+import java.math.BigDecimal;
+
+public class Share {
+    private final Stock stock;
+    private final BigDecimal quantity;
+    private final BigDecimal purchasePrice;
+
+    public Share(Stock stock, BigDecimal quantity, BigDecimal purchasePrice) {
+        if (stock == null) {
+            throw new IllegalArgumentException("Stock cannot be null");
+        }
+        if (quantity == null || quantity.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Quantity must be positive");
+        }
+        if (purchasePrice == null || purchasePrice.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Purchase price must positive");
+        }
+
+        this.stock = stock;
+        this.quantity = quantity;
+        this.purchasePrice = purchasePrice;
+    }
+
+    public Stock getStock() {
+        return stock;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public BigDecimal getPurchasePrice() {
+        return purchasePrice;
+    }
+}
+
