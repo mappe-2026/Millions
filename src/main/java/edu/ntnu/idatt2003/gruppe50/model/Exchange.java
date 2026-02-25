e8ffe797c165f3913e9fc029ff92382ae88f9624
Kristian <vestboe.kristian@gmail.com>
2026-02-25 13:40:48 +0100

    Finish Exchange class

--- src/main/java/edu/ntnu/idatt2003/gruppe50/model/Exchange.java
+++ src/main/java/edu/ntnu/idatt2003/gruppe50/model/Exchange.java
@@ -1,11 +1,16 @@
 package edu.ntnu.idatt2003.gruppe50.model;
 
+import edu.ntnu.idatt2003.gruppe50.transaction.Purchase;
+import edu.ntnu.idatt2003.gruppe50.transaction.Sale;
+import edu.ntnu.idatt2003.gruppe50.transaction.Transaction;
+
+import java.math.BigDecimal;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Exchange {
     private final String name;
-    private final int week;
+    private int week;
     private final Map<String, Stock> stockMap;
     private final Random random;
 
@@ -49,5 +54,50 @@ public class Exchange {
                 ).toList();
     }
 
+    public Transaction buy(String symbol, BigDecimal quantity, Player player) {
+        if (!hasStock(symbol)) {
+            throw new NoSuchElementException("No stock by that symbol");
+        }
+        if (quantity.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("quantity cannot be less than 1");
+        }
+        if (player == null) {
+            throw new IllegalArgumentException("Player cannot be null");
+        }
+
+        Stock stock = getStock(symbol);
+        Share share = new Share(stock, quantity, stock.getSalesPrice());
+        Purchase purchase = new Purchase(share, this.week);
+        purchase.commit(player);
+
+        return purchase;
+    }
+
+    public Transaction sell(Share share, Player player) {
+        if (share == null) {
+            throw new IllegalArgumentException("Share cannot be null");
+        }
+        if (player == null) {
+            throw new IllegalArgumentException("Player cannot be null");
+        }
+
+        Sale sale = new Sale(share, this.week);
+        sale.commit(player);
+
+        return sale;
+    }
+
+    public void advance() {
+        this.week++;
+
+        stockMap.replaceAll((symbol, stock) -> {
+            double change = random.nextDouble()*0.4;
+            BigDecimal multiplier = BigDecimal.valueOf(1 + change);
+
+            BigDecimal newPrice = stock.getSalesPrice().multiply(multiplier);
+            stock.addNewSalesPrice(newPrice);
+            return stock;
+        });
+    }
 
 }
