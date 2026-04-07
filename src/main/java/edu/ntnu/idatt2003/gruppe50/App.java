7c482d88ac2a4cae5d3374df0cd58c6d3ea5c610
Marius Klepp <Mariusklepp@icloud.com>
2026-04-07 15:35:16 +0200

    Added javafx to app

--- src/main/java/edu/ntnu/idatt2003/gruppe50/App.java
+++ src/main/java/edu/ntnu/idatt2003/gruppe50/App.java
@@ -1,13 +1,26 @@
 package edu.ntnu.idatt2003.gruppe50;
 
+import edu.ntnu.idatt2003.gruppe50.view.NewGameView;
+import javafx.application.Application;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App extends Application {
+
+    private BorderPane mainLayout;
+
+    public static void main( String[] args ) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        NewGameView newGame = new NewGameView(stage);
+        stage.setScene(newGame.getScene());
+        stage.show();
     }
 }
