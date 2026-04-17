7782da1b7def0e156f2ce8e8e93423597c3401f1
Kristian <vestboe.kristian@gmail.com>
2026-04-17 16:13:09 +0200

    feat: add basic portfolio view, add dependency injection to GameView and NavigationManager

--- src/main/java/edu/ntnu/idatt2003/gruppe50/view/pages/GameView.java
+++ src/main/java/edu/ntnu/idatt2003/gruppe50/view/pages/GameView.java
@@ -1,14 +1,23 @@
 package edu.ntnu.idatt2003.gruppe50.view.pages;
 
+import edu.ntnu.idatt2003.gruppe50.model.Exchange;
+import edu.ntnu.idatt2003.gruppe50.model.Player;
 import edu.ntnu.idatt2003.gruppe50.view.components.NavBar;
 import edu.ntnu.idatt2003.gruppe50.view.navigation.NavigationManager;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 public class GameView{
+  private final Player player;
+  private final Exchange exchange;
+
+  public GameView(Player player, Exchange exchange) {
+    this.player = player;
+    this.exchange = exchange;
+  }
 
   public Scene getScene() {
-    NavigationManager navManager = new NavigationManager();
+    NavigationManager navManager = new NavigationManager(player, exchange);
     NavBar navBar = new NavBar(navManager::navigateTo);
 
     BorderPane root = new BorderPane();
---
+++ src/main/java/edu/ntnu/idatt2003/gruppe50/view/pages/PortfolioView.java
@@ -0,0 +1,43 @@
+package edu.ntnu.idatt2003.gruppe50.view.pages;
+
+import edu.ntnu.idatt2003.gruppe50.model.Player;
+import edu.ntnu.idatt2003.gruppe50.model.Portfolio;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class PortfolioView extends VBox implements Page {
+
+  public PortfolioView(Player player) {
+    Portfolio portfolio = player.getPortfolio();
+
+    // Make label for the different player values
+    Label portfolioValueLabel = new Label(portfolio.getNetWorth().toString());
+    Label playerCashLabel = new Label(player.getMoney().toString());
+    Label netWorthLabel = new Label(player.getNetWorth().toString());
+
+    // Make cards for the player information
+    VBox portfolioValueCard = new VBox(new Label("Portfolio value:"), portfolioValueLabel);
+    VBox cashBalanceCard = new VBox(new Label("Cash balance:"), playerCashLabel);
+    VBox netWorthCard = new VBox(new Label("Net worth:"), netWorthLabel);
+
+    // Make container for the different cards
+    VBox cardContainer = new VBox(portfolioValueCard, cashBalanceCard, netWorthCard);
+
+    // Make the container for the header section
+    HBox topSection = new HBox(cardContainer);
+
+    this.getChildren().add(topSection);
+  }
+
+  @Override
+  public Parent getView() {
+    return this;
+  }
+
+  @Override
+  public String getTitle() {
+    return "Portfolio";
+  }
+}
