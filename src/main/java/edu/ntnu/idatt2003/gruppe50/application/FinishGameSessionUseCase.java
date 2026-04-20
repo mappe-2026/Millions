1455b293fcea00da52f8b5da5456e477d9a5a6d9
Kristian <vestboe.kristian@gmail.com>
2026-04-20 14:29:57 +0200

    feat(application): add use-cases for finish game command

---
+++ src/main/java/edu/ntnu/idatt2003/gruppe50/application/FinishGameSessionUseCase.java
@@ -0,0 +1,24 @@
+package edu.ntnu.idatt2003.gruppe50.application;
+
+import edu.ntnu.idatt2003.gruppe50.domain.game.GameSession;
+import edu.ntnu.idatt2003.gruppe50.domain.repository.GameSessionRepository;
+
+import java.util.UUID;
+
+public final class FinishGameSessionUseCase {
+  private final GameSessionRepository repository;
+
+  public FinishGameSessionUseCase(GameSessionRepository repository) {
+    this.repository = repository;
+  }
+
+  public void execute(Request request) {
+    GameSession session = repository.findById(request.gameId())
+        .orElseThrow(GameSessionNotFoundException::new);
+
+    session.finish();
+    repository.save(session);
+  }
+
+  public record Request(UUID gameId) {}
+}
